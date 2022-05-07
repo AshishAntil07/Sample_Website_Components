743f90a773b92ac41cab80ba493444218ad38d5f
AshishAntil07 <98260686+AshishAntil07@users.noreply.github.com>
2022-05-07 16:17:56 +0530

    Create StyleSheet.js

---
+++ Drop-Down with NavBar/StyleSheet.js
@@ -0,0 +1,353 @@
+*{
+  margin: 0;
+  padding: 0;
+}
+
+html{
+  box-sizing: border-box;
+}
+.wrapper{
+  width: 100%;
+}
+.Head-container{
+  background-color: white;
+  height: auto;
+} 
+.Head-container .logo{
+  width: 11%;
+  height: 65px;
+  margin: 0.3%;
+  margin-left: 5%;
+}
+.Head-container .registerer{
+  margin-left: 85%;
+}
+.Head-container .registerer .pad-helper{
+  font-size: 25px;
+  cursor: default;
+  padding-top: 20px;
+  padding: 21px;
+  width: 110px;
+  background-color: transparent;
+  padding-left: 5px;
+  padding-right: 5px; 
+  font-family: sans-serif;
+}
+.Head-container .registerer .pad-helper::selection{
+  background-color: transparent;
+  color: black;
+}
+.Head-container .registerer ul{
+  list-style: none;
+}
+.Head-container .registerer ul li ul{
+  background-color: white;
+  display: none;
+  margin-top: -15px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+  width: 175px;
+  z-index: 20;
+  position: absolute;
+}
+.Head-container .registerer ul li ul .arrow-img{
+  transform: rotate(45deg);
+  margin-left: 30px;
+  position: absolute;
+  margin-top: -7px;
+}
+.Head-container .registerer ul li ul .arrow-img::selection{
+  background-color: transparent;
+  color: transparent;
+}
+.Head-container .registerer ul li ul li:nth-child(2){
+  border-bottom: 1px solid black;
+  border-left: 3px solid gray;
+  transition: border .4s;
+  position: relative;
+  z-index: 10;
+}
+.Head-container .registerer ul li ul #seleChildReveLog::selection{
+  background-color: transparent;
+  color: black;
+}
+.Head-container .registerer ul li ul li:nth-child(2):hover{
+  border-left: 3px solid red;
+  color: rgb(200, 0, 0);
+}
+.Head-container .registerer ul li ul li:last-child{
+  transition: border .4s;
+  border-left: 3px solid grey;
+  border-top: 1px solid black;
+  position: relative;
+  z-index: 10;
+}
+.Head-container .registerer ul li ul #seleChildReveSign::selection{
+  background-color: transparent;
+  color: black;
+}
+.Head-container .registerer ul li ul li:last-child:hover{
+  border-left: 3px solid red;
+}
+.Head-container .registerer ul li ul li{
+  padding: 7px;
+}
+.Head-container .registerer ul li ul li a{
+  text-decoration: none;
+  font-family: sans-serif;
+  color: black;
+  font-size: 20px;
+  transition: color .2s;
+}
+.Head-container .registerer ul li ul li a:last-child:hover{
+  color:rgb(200, 0, 0);
+}
+.Head-container .registerer ul li ul .log-btn, .Head-container .registerer ul li ul .sign-btn{
+  line-height: 22.5px;
+}
+.Head-container .registerer ul li ul .log-btn, .Head-container .registerer ul li ul .log-btn{
+  line-height: 22.5px;
+}
+.nav-bar{
+  box-sizing: content-box;
+  width: 100%;
+  height: 66px;
+  overflow: show;
+  background-color: rgb(20, 20, 20);
+  display: flex;
+  justify-content: center;
+  outline: #343a407c solid 2px;
+  z-index: 1000;
+}
+.nav-bar .tripletsAligner{
+  box-sizing: border-box;
+  height: 66px;
+  display: flex;
+  align-items: center;
+  margin-left: 265px;
+}
+.nav-bar ul{
+  color: gray;
+  transition: color .4s;
+  font-family: sans-serif;
+}
+.nav-bar ul:hover{
+  color: lightgray;
+}
+.nav-bar ul li{
+  list-style: none;
+  text-align: center;
+}
+.nav-bar ul li a{
+  color: gray;
+  transition: color .4s;
+  text-decoration: none;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.nav-bar .tripletsAligner ul li{
+  color: grey;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  transition:color .4s, letter-spacing .4s, font-weight .4s;
+}
+.nav-bar .tripletsAligner ul li:hover{
+  color: lightgray;
+  letter-spacing: 2px; 
+  font-weight: 600;
+}
+.nav-bar #space-reducer1{
+  width: 115px;
+}
+.nav-bar #space-reducer2{
+  width: 142.7px;
+}
+.nav-bar #space-reducer3{
+  width: 95.1px;
+}
+#space-reducer1, #space-reducer2, #space-reducer3, #space-reducer4{
+  height: 66px;
+}
+#space-reducer1 li, #space-reducer2 li, #space-reducer3 li, #space-reducer4 .PTBAligner{
+  height: inherit;
+  cursor: pointer;
+}
+#space-reducer1 li::before, #space-reducer2 li::before, #space-reducer3 li::before{
+  bottom: 18px;
+}
+#space-reducer1 li::after, #space-reducer2 li::after, #space-reducer3 li::after{
+  bottom: -21px;
+}
+#space-reducer1 li::after, #space-reducer2 li::after, #space-reducer3 li::after,
+#space-reducer1 li::before, #space-reducer2 li::before, #space-reducer3 li::before{
+  display: block;
+  position: relative;
+  background-color: lightgray;
+  width: 0px;
+  height: 3px;
+  content: " ";
+  margin-left: 50%;
+  margin-right: 50%;
+  transition: width .4s, margin-left .4s, margin-right .4s;
+}
+#space-reducer3 li:hover::before, #space-reducer3 li:hover::after{
+  width: 95.1px;
+  margin-left: 0%;
+  margin-right: 0%;
+}
+#space-reducer1 li:hover::before, #space-reducer1 li:hover::after{
+  width: 115px;
+  margin-left: 0%;
+  margin-right: 0%;
+}
+#space-reducer2 li:hover::before, #space-reducer2 li:hover::after{
+  width: 142.7px;
+  margin-left: 0%;
+  margin-right: 0%;
+}
+.nav-bar #space-reducer4{
+  width: 200px;
+  margin-left: 15px;
+}
+.nav-bar #space-reducer4 .PTBAligner{
+  cursor: default;
+  padding-top: 23px;
+  padding-bottom: 23px;
+}
+.nav-bar ul li .child{
+  display: none;
+  width: 175px;
+  background: rgb(20, 20, 20);
+  border-bottom: 2px solid rgb(40, 40, 40);
+  margin-left: 10px;
+  margin-top: -15px;
+  z-index: 10;
+  border: 1px solid lightgrey;
+  position: relative;
+}
+.nav-bar ul li .child ul{
+  height: 280px;
+  padding-right: 5px;
+  padding-left: 5px;
+  box-shadow: 0 4px 8px 0 rgb(20, 20, 20);
+}
+.nav-bar ul li:hover .child, .nav-bar ul li i:hover .child{
+  display: block;
+}
+.nav-bar ul li:hover .child ul, .nav-bar ul li i:hover .child ul{
+  display: block;
+}
+.nav-bar ul li:hover .child ul li{
+  border-bottom: 1px dotted white;
+  line-height: 39px;
+}
+.nav-bar ul li:hover .child ul li a{
+  letter-spacing: normal;
+  font-weight: normal;
+  color: gray;
+  transition: color .4s;
+}
+.nav-bar ul li:hover .child ul li a:hover{
+  color: lightgray;
+}
+.nav-bar ul li:hover .child ul li:last-child{
+  border-bottom: none;
+}
+.Se-launcher{
+  width: 300px;
+}
+.Se-launcher::selection{
+  background-color: transparent;
+  color: white;
+}
+.Se-representator{
+  box-sizing: content-box;
+  height: 45px;
+  width: 45px;
+  border-radius: 5px;
+  margin: 4px;
+  margin-left: 300px;
+  background-color: rgb(5, 5, 5);
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  overflow: hidden;
+  border: 1px solid rgb(49, 49, 49);
+  transition: width ease-in-out 0.4s, margin ease-in-out 0.4s;
+}
+.bg-helper{
+  box-sizing: content-box;
+  background-color: rgb(20, 20, 20);
+  height: 45px;
+  width: 45px;
+  background-repeat: no-repeat;
+  border-radius: 5px;
+  background-image: url("Pictures/Magnifying Glass(optional).png");
+  background-size: 45px 45px;
+}
+.bg-helper:hover{
+  background-color: rgb(40, 40, 40);
+}
+.bg-helper:active{
+  background-color: rgb(30, 30, 30);
+}
+.proSearcherc{
+  box-sizing: content-box;
+  width: 80px;
+  height: 22.5px;
+  display: none;
+  border-radius: 5px;
+  margin-top: 2.5px;
+  margin-right: 5px;
+  margin-left: 5px;
+  background-color: grey;
+  color: white;
+  font-size: 20px;
+  padding: 9px;
+  padding-left: 20px;
+  font-family: sans-serif;
+  border: 1px solid black;
+  cursor: default;
+}
+.proSearcherc::selection{
+  background-color: transparent;
+  color: white;
+}
+.proSearcherc:hover{
+  background-color: rgb(141, 141, 141);
+}
+.proSearcherc:active{
+  background-color: dimgrey;
+  transition: none;
+}
+#search::selection{
+  color: grey;
+  background-color: #014792;
+}
+.Se-launcher #search{
+  display: none;
+  box-sizing: content-box;
+  width: 290px;
+  height: 38px;
+  border-radius: 5px;
+  padding-left: 37.5px;
+  padding-right: 10px;
+  font-size: 16px;
+  color: grey;
+  background-size: 32.5px 32.5px;
+  background-position: left;
+  background-repeat: no-repeat;
+  background-color: transparent;
+  background-image: url("Pictures/Search Icon.png");
+  border-top: none; border-right: none; border-left: none;
+  border-bottom: 2px solid grey;
+  transition: width ease-in-out .4s, background-image .4s, border .4s, padding-right .4s;
+}
+.Se-launcher #search:focus{
+  background-image: url("Pictures/Search Icon2.png");
+  border-bottom: 2px solid #014792;
+  outline: none;
+}
+#SPrinter, #noGiver{
+  position: absolute;
+  visibility: hidden;
+}
